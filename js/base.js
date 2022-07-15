8edba004b92ea16667fa13df924523c763beb512
5720572 <5720572@naver.com>
2022-07-15 14:13:50 +0900

    웹기능사 상단 슬라이드 까지

--- js/base.js
+++ js/base.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-    
-})
+    // //slide
+    // $('body').addClass('cccc')
+    // //요소검사 -body태그 클래스 확인해보기
+    // setInterval = 특정시간단위로 주기적으로 처리
+    // setTimeout = 특정시간이 흐르고 한번 실행
+
+    // var count = 0;
+    // setInterval(function(){
+    //     count++;
+    //     count %= 3;
+    //     $('.aniSlide').animate({
+    //         "marginLeft" : -1200 * count
+    //     }, 400, function(){})
+    // } , 3000)
+    // })
+
+    setInterval(function(){
+        $('.aniSlide').animate({
+            "marginLeft" : "-=1200"
+        },400,function(){
+            $('.aniSlide > div').eq(0).appendTo($(this))
+            $(this).css('margin-Left',0)
+        })
+        }, 3000)
+    })
