4773f643ab32e2a6ef2fd2174ecf425d277ef761
5720572 <5720572@naver.com>
2022-07-18 11:22:40 +0900

    clean push

--- js/base.js
+++ js/base.js
@@ -10,16 +10,10 @@
 //     }, 3000)
 // })
 $(document).ready(function(){
-
-    setInterval(aniSlide, 3000)
-
-     function aniSlide(){
-        $('.aniSlide').animate({
-            "marginLeft" : "-=1200"
-        }, 400, function(){
-            $('.aniSlide > div').eq(0).appendTo($('.aniSlide'))
-            $('.aniSlide').css('marginLeft',0)
-        })
-    }
-
+    var targetdiv = 0;
+    setInterval(function(){
+        targetdiv++;
+        targetdiv %= 3;
+        $('.aniSlide > div').eq(targetdiv).addClass('on').siblings().removeClass('on')
+    }, 3000)
 })
